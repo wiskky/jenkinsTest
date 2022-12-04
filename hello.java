2c07cae6ff52c67529b599a3f72fd3f76b0e4c68
wiskky <adeniyi.wisdom@gmail.com>
2022-12-04 19:21:09 +0100

    Updating class name

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 // Your First Program
 
-class HelloWorld {
+class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
 	System.out.println("This is for testing on Jenkins");
